cc0a650407ed5b144f51ca29d0e0b48ec8378f9f
Blssel <1109047248@qq.com>
2018-05-23 16:05:43 +0800

    新增kinetics数据集解析函数

--- download-vid-by-url/parse_dataset/parse_fun.py
+++ download-vid-by-url/parse_dataset/parse_fun.py
@@ -19,10 +19,16 @@ def parse_activitynet(json_file):
 
 
 def parse_kinetics(json_file):
-    return
+    with open(json_file,'r') as f:
+        json_data=json.load(f)
+
+    vid_list=[]
+    for vid_id in json_data.keys():
+        id_url=vid_id+ '  '+ json_data[vid_id]['url']
+        vid_list.append(id_url)
+
+    return vid_list
 
 
 def parse_ava(json_file):
     return
-
-
