8dd988c23522f636b1d435dc58c98adee5991c52
Blssel <1109047248@qq.com>
2018-05-23 16:05:22 +0800

    修改failed文件未定义错误

--- download-vid-by-url/download/download.py
+++ download-vid-by-url/download/download.py
@@ -1,6 +1,6 @@
-# coding:UTF-8
+#coding:utf-8
 
-__author__='zhiyu yin'
+uthor__='zhiyu yin'
 
 import sys
 import argparse
@@ -12,24 +12,26 @@ from parse_dataset import parse_json_file
 def download(id_url):
     vid_id =id_url.split()[0]
     vid_url=id_url.split()[1]
-    
+      
     # 依据URL，下载数据（自动跳过并记录失败的数据）    
     # 选项解释https://github.com/rg3/youtube-dl/blob/3e4cedf9e8cd3157df2457df7274d0c842421945/youtube_dl/YoutubeDL.py#L137-L312
     ydl_opts={
         'outtmpl':vid_id+'.mp4',
         #'format': 'bestvideo+bestaudio/best',
         # 'verbose': True,  # Print additional info to stdout.
-        'proxy': '127.0.0.1:8123',
+        #'proxy': '127.0.0.1:8123',
         #'preferredquality': '480',
     }
-
+  
     with youtube_dl.YoutubeDL(ydl_opts) as ydl:
         try:
             ydl.extract_info(vid_url,download=True)
         except:
-            print("!!!!!!------Download fail.Automatically ignoring and recoding its info------!!!!!!!")
-            with open(failed_file,'a') as err:
+            print("!!!!!!------Download failed. Automatically ignoring and recoding its info------!!!!!!!")
+            with open(args.failed_file,'a') as err:
                 err.write(vid_id+ '  '+ vid_url+ '\n')
+
+
 
 # 解析参数
 def parse_args():
@@ -42,8 +44,10 @@ def parse_args():
     
     return args
 
+
 def main():
     # 解析参数
+    global args
     args=parse_args()
     
     # 读取json或其他格式文件中的数据至一个video_list，video_list中的每一项都是一个ID和URL组合成的数组(id+'  '+url)
